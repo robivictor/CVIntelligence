0e4bd761b47daf52889763f7b8fd48da01c1d0e4
robi-github <blessedethio@gmail.com>
2015-02-04 09:00:09 +0300

    polish the CV view

--- CVIntelligence/App_Start/BundleConfig.cs
+++ CVIntelligence/App_Start/BundleConfig.cs
@@ -10,7 +10,10 @@ namespace CVIntelligence
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/myscripts").Include(
+                       "~/Scripts/highlight.pack.js",
+                       "~/Scripts/bootstrap-filestyle.min.js"
+                       ));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -22,7 +25,9 @@ namespace CVIntelligence
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/solarized_light.css"
+                      ));
         }
     }
 }
